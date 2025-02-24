20b6f702a8a449a343aab1ea0d4531195b23c4d1
priyadharshiniV-15 <dharshiniragavan15@gmail.com>
2025-02-24 18:52:29 +0530

    Create Task 1.java

---
+++ 22-02-2025 (Core Java)/Task 7-Collection API/Task 1.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColorList {
+    public static void main(String[] args) {
+        List<String> colors = new ArrayList<>();
+
+       
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Purple");
+
+        
+        System.out.println("Color List: " + colors);
+    }
+}
