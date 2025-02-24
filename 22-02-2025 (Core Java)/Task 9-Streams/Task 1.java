4613fa0e9a3f2697ce7d657dec2cbcc17c4a0a6f
priyadharshiniV-15 <dharshiniragavan15@gmail.com>
2025-02-24 19:47:06 +0530

    Update and rename Task 1 to Task 1.java

---
+++ 22-02-2025 (Core Java)/Task 9-Streams/Task 1.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class AverageCalculator {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
+        double average = numbers.stream().mapToInt(Integer::intValue).average().orElse(0);
+        System.out.println("Average: " + average);
+    }
+}
+
