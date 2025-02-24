25c884a9471aef91d345548c8b3d84afcaab8d67
priyadharshiniV-15 <dharshiniragavan15@gmail.com>
2025-02-24 19:47:42 +0530

    Create Task 2.java

---
+++ 22-02-2025 (Core Java)/Task 9-Streams/Task 2.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StringCaseConverter {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("apple", "banana", "cherry");
+
+        List<String> upperCaseWords = words.stream().map(String::toUpperCase).collect(Collectors.toList());
+        List<String> lowerCaseWords = words.stream().map(String::toLowerCase).collect(Collectors.toList());
+
+        System.out.println("Uppercase: " + upperCaseWords);
+        System.out.println("Lowercase: " + lowerCaseWords);
+    }
+}
