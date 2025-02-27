c91c97a6a12b27707e76328e3586b363a94a7714
priyadharshiniV-15 <dharshiniragavan15@gmail.com>
2025-02-27 16:48:43 +0530

    Create main.jsx

---
+++ 27-02-2025/Task 2/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import C1 from "./C1";
+createRoot(document.getElementById("root")).render(
+  
+  <StrictMode>
+    <h2><C1 /></h2>
+  </StrictMode>
+);  
