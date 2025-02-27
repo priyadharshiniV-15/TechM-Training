e3f31a29e4206c03fe4539e420548499c3aca6cc
priyadharshiniV-15 <dharshiniragavan15@gmail.com>
2025-02-27 16:43:18 +0530

    Create Component 5.jsx

---
+++ 27-02-2025/Task 1/Component 5.jsx
@@ -0,0 +1,16 @@
+function Component5(props) {
+    return (
+      <>
+    <p>Email : {props.email}</p>
+    <Contact/>
+    </>
+  );
+  }
+  function Contact() {
+    return (
+      <a href="https://www.linkedin.com/in/priyadharshini-vijayaragavan-637b2624a/">Contact</a>
+    );
+  }
+  
+  export default Component5
+  
